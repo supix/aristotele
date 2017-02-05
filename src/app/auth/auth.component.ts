59689f524f87074abd1a1a92e77f7a421aaa65f2
supix <esposito.marce@gmail.com>
2017-02-05 22:10:52 +0100

    Authentication form
    Authentication form has full validation constraints.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  FormBuilder,
+  FormGroup,
+  AbstractControl,
+  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  email: string;
+  form: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) { 
+    this.email = '';
+
+    this.form = fb.group({
+      'email': ['', Validators.required],
+      'password': ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(20)])]
+    });
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(value): void {
+    this.email = value.email;
+    //console.log(this.getEmail(), " ", this.isLoggedIn());
+  }
+
+  ctrl(name: string): AbstractControl {
+    return this.form.controls[name];
+  }
+
+  public isLoggedIn(): boolean {
+    return this.email != '';
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
 }
